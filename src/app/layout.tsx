b0c7e4c58c1bff6019e0fe93fb0a8b5ff4cb8056
suradet16 <suradetfriend16@gmail.com>
2024-03-21 04:03:42 +0700

    Navbar + Register

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Nunito } from "next/font/google";
+import Navbar from "@/components/navbar/Navbar";
+import ClientOnly from "../components/ClientOnly";
+import Modal from "@/components/modals/Modal";
+import RegisterModal from "@/components/modals/RegisterModal";
+import ToasterProvider from "@/providers/ToasterProvider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Dentist Booking",
+  description: "Dentist Booking Website",
+};
+
+const font = Nunito({
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider/>
+          <RegisterModal/>
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
+    </html>
+  );
+}
