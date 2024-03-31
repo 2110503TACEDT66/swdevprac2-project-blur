c86dffdc345479e51f35b63f4e33e75546df4d13
suradet16 <suradetfriend16@gmail.com>
2024-03-31 10:15:35 +0700

    deploy

--- src/libs/userRegister.tsx
+++ src/libs/userRegister.tsx
@@ -1,6 +1,6 @@
 export default async function userRegister(userName:string, userTel:string, userEmail:string, userPassword:string){
 
-  const response = await fetch(`${process.env.BACKEND_URL}/api/v1/auth/register`, {
+  const response = await fetch(`${process.env.BACKEND_URL}api/v1/auth/register`, {
       method: "POST",
       headers: {
           "Content-Type": "application/json",
